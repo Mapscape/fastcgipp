1e7ea08f590c1814d7e49b9b88bb27c3f1937053
Eddie Carle <eddie@isatec.ca>
2016-03-04 13:23:25 -0700

    Started segregating of the OS level sockets
    The OS level sockets code used to be contained in the Transceiver class.
    They have been rewritten and moved into their own interface to make it
    much easier to port the library to non-GNU/Linux operating systems.

---
+++ include/fastcgi++/sockets.hpp
@@ -0,0 +1,331 @@
+/*!
+ * @file       sockets.hpp
+ * @brief      Declares everything for interfaces with OS level sockets.
+ * @author     Eddie Carle &lt;eddie@isatec.ca&gt;
+ * @date       March 4, 2016
+ * @copyright  Copyright &copy; 2016 Eddie Carle. This project is released under
+ *             the GNU General Public License Version 3.
+ *
+ * It is this file, along with sockets.cpp, that must be modified to make
+ * fastcgi++ work on any non-linux operating system. Although not completely
+ * sure, I believe all that needs to be modified in this file is
+ * Fastcgipp::socket_t and Fastcgipp::poll_t.
+ */
+
+/*******************************************************************************
+* Copyright (C) 2016 Eddie Carle [eddie@isatec.ca]                             *
+*                                                                              *
+* This file is part of fastcgi++.                                              *
+*                                                                              *
+* fastcgi++ is free software: you can redistribute it and/or modify it under   *
+* the terms of the GNU Lesser General Public License as  published by the Free *
+* Software Foundation, either version 3 of the License, or (at your option) any*
+* later version.                                                               *
+*                                                                              *
+* fastcgi++ is distributed in the hope that it will be useful, but WITHOUT ANY *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR*
+* A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more    *
+* details.                                                                     *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with fastcgi++.  If not, see <http://www.gnu.org/licenses/>.           *
+*******************************************************************************/
+
+#ifndef SOCKETS_HPP
+#define SOCKETS_HPP
+
+#include <memory>
+#include <map>
+#include <mutex>
+
+//! Topmost namespace for the fastcgi++ library
+namespace Fastcgipp
+{
+    //! Our socket identifier type in GNU/Linux is simply an int
+    typedef int socket_t;
+
+    //! Our polling type in GNU/Linux is simply an int
+    typedef int poll_t;
+
+    class Listener;
+
+    //! Class for representing an OS level I/O socket.
+    /*!
+     * It works together with the Listener class to establish all the
+     * interfacing between the OS and the Transceiver class to communicate with
+     * the outside world. The objects of this class represent individual
+     * connections to the FastCGI server. They are consolidated and managed
+     * within the Listener class.
+     *
+     * <em>No non-const member functions are thread safe. This means you can
+     * only use valid() and the comparison operators across multiple threads.
+     * </em>
+     *
+     * @date    March 4, 2016
+     * @author  Eddie Carle &lt;eddie@isatec.ca&gt;
+     */
+    class Socket
+    {
+    private:
+        //! Our respective Listener needs private access.
+        friend class Listener;
+
+        //! Data structure to hold the shared socket data.
+        struct Data
+        {
+            //! OS level socket identifier.
+            const socket_t m_socket;
+
+            //! Indicates whether or not the socket is dead/invalid.
+            bool m_valid;
+
+            //! Listener object this socket is tied to.
+            Listener& m_listener;
+
+            //! Sole constructor
+            /*!
+             * @param [inout] socket The OS level socket identifier to associate
+             *                       with this.
+             * @param [inout] listener The Listener object that created and is
+             *                         consolidating this socket and it's peers.
+             * @param [in] valid Set to false if this is a dead socket.
+             *                   Sometimes it may be needed to create a socket
+             *                   that is invalid off the start.
+             */
+            Data(
+                    const socket_t& socket,
+                    bool valid,
+                    Listener& listener):
+                m_socket(socket),
+                m_valid(valid),
+                m_listener(listener)
+            {}
+
+            Data() =delete;
+            Data(const Data&) =delete;
+        };
+
+        //! Shared pointer to hold the socket data.
+        const std::shared_ptr<Data> m_data;
+
+        //! This is only true for a non-copy constructed object.
+        bool m_original;
+
+        //! Sole non-copy/move constructor
+        /*!
+         * This constructor is only accessible to the Listener class to create
+         * new "original" sockets as they are accepted. Only sockets created
+         * with this constructor will have m_original set to true.
+         *
+         * @param [inout] socket The OS level socket identifier to associate
+         *                       with this.
+         * @param [inout] listener The Listener object that created and is
+         *                         consolidating this socket and it's peers.
+         * @param [in] valid Set to false if this is a dead socket. Sometimes it
+         *                   may be needed to create a socket that is invalid
+         *                   off the start.
+         */
+        Socket(
+                const socket_t& socket,
+                Listener& listener,
+                bool valid=true);
+
+        Socket() =delete;
+
+    public:
+        //! Try and read a chunk of data out of the socket.
+        /*!
+         * This function will attempt to read the requested amount of data out
+         * of the socket into the buffer. The return value indicates how many
+         * bytes were actually read.
+         *
+         * If an error occurs during the read operation the socket is
+         * closed/destroyed and marked invalid. A size of zero is returned in
+         * case of an error but does not indicate an error. Zero could also be
+         * returned if there is simply no data to read as this function is
+         * non-blocking. To test for error simply use the valid() function.
+         *
+         * @param [out] buffer Pointer to memory location to which data should
+         *                     be read into.
+         * @param [in] size Maximum amount of data to read into the buffer.
+         *                  Obviously this should be less than or equal to the
+         *                  actual amount of memory allocated in the buffer.
+         * @return Actual number of bytes read into the buffer.
+         */
+        size_t read(char* buffer, size_t size);
+
+        //! Try and write a chunk of data into the socket.
+        /*!
+         * This function will attempt to write the requested amount of data into
+         * the socket from the buffer. The return value indicates how many bytes
+         * were actually written.
+         *
+         * If an error occurs during the write operation the socket is
+         * closed/destroyed and marked invalid. A size of zero is returned in
+         * case of an error but does not indicate an error. Zero could also be
+         * returned if there is simply no room to write data as this function is
+         * non-blocking. To test for error simply use the valid() function.
+         *
+         * @param [out] buffer Pointer to memory location to which data should
+         *                     be written from.
+         * @param [in] size Maximum amount of data to write from the buffer.
+         * @return Actual number of bytes written from the buffer.
+         */
+        size_t write(const char* buffer, size_t size);
+
+        //! We need this to allow the socket objects to be in sorted containers.
+        bool operator<(const Socket& x) const
+        {
+            return m_data < x.m_data;
+        }
+
+        //! We need this to allow the socket objects to be in sorted containers.
+        bool operator==(const Socket& x) const
+        {
+            return m_data == x.m_data;
+        }
+
+        //! Copy constructor
+        /*!
+         * Any sockets built using the copy constructor will not be marked
+         * original.
+         */
+        Socket(const Socket& x):
+            m_data(x.m_data),
+            m_original(false)
+        {}
+
+        //! Move constructor
+        /*!
+         * This constructor serves the purposes of moving "original" sockets
+         * into containers. The source socket has it's originality stripped and
+         * moved to the destination.
+         */
+        Socket(Socket&& x):
+            m_data(x.m_data),
+            m_original(x.m_original)
+        {
+            x.m_original=false;
+        }
+
+        //! Calls close() on the socket if we are destructing the original
+        ~Socket()
+        {
+            if(m_original)
+                close();
+        }
+
+        //! Returns true if this socket is still open and capable of read/write.
+        bool valid() const
+        {
+            return m_data->m_valid;
+        }
+
+        //! Call this to close the socket
+        /*!
+         * If the socket is valid, this will do the following:
+         *  - Close/hangup the OS level socket.
+         *  - Remove the socket from the associated Listener polling set.
+         *  - Fully disassociate the socket with the Listener.
+         *  - Mark the socket as invalid.
+         *
+         * If the socket is already invalid, calling this does nothing.
+         */
+        void close();
+    };
+
+    //! Class for representing an OS level socket that listens for connections.
+    /*!
+     * It works together with the Socket class to establish all the interfacing
+     * between the OS and the Transceiver class to communicate with the outside
+     * world. The object of this class represents the socket that listens for
+     * incoming connections to the FastCGI server. This class will create and
+     * manage an array of Socket objects as new connections are initiated.
+     *
+     * <em>The only part of this class that is safe to call from multiple
+     * threads is the wake() function.</em>
+     *
+     * @date    March 4, 2016
+     * @author  Eddie Carle &lt;eddie@isatec.ca&gt;
+     */
+    class Listener
+    {
+    public:
+        //! Constructor
+        /*!
+         * Construct a Listener object based on an initial socket identifier to
+         * listen on.
+         *
+         * @param[in] listen Socket identifier to listen for connections on
+         */
+        Listener(socket_t listen);
+
+        //! Poll socket set for new incoming connections and data.
+        /*!
+         * Calling this will initiate a poll for both new connections and new
+         * incoming data within the set of sockets that this object has created.
+         * It will also keep an eye out for dead sockets and tear them down
+         * accordingly.
+         *
+         * Its behaviour is as follows:
+         *  - If a new connection arrives, it will establish the new connection.
+         *  - If a socket is dead, it destroys and marks the socket as invalid.
+         *    It will not return anything regarding the dead socket.
+         *  - If new data has arrived in a currently active connection it will
+         *    return the socket.
+         *  - If the call has been set to non-blocking and no new data awaits, a
+         *    generic invalid socket is returned.
+         *
+         * This function can be either blocking or non-blocking depending on the
+         * boolean value passed to it. If the call is blocking it can be awoken
+         * in a thread safe manner with a call to wake().
+         *
+         * @param[in] block Set \em true to make the call sleep and wait for new
+         *                  data to arrive.
+         * @return The socket for which there is new data waiting. Make sure to
+         *         Socket::valid() on it to ensure validity.
+         */
+        Socket poll(bool block);
+
+        //! Wake up from a nap inside poll()
+        /*!
+         * Calling this simply wakes up the execution thread that poll() is
+         * blocking in. If there is no nap taking place, calling this does
+         * nothing.
+         *
+         * This function is thread safe and can be called from anywhere as often
+         * as is desired.
+         */
+        void wake();
+
+    private:
+        //! Our sockets need access to our private data
+        friend class Socket;
+
+        //! This is the file descriptor we listen for connections on
+        const socket_t m_listen;
+
+        //! Our poll object
+        const poll_t m_poll;
+
+        //! A pair of sockets for wakeup purposes
+        socket_t m_wakeSockets[2];
+
+        //! Set to true while sleeping
+        bool m_sleeping;
+
+        //! We need this mutex to thread safe the wake() function.
+        std::mutex m_sleepingMutex;
+
+        //! All the sockets
+        std::map<socket_t, Socket> m_sockets;
+
+        //! Generic bad socket
+        const Socket m_badSocket;
+
+        //! Accept a new connection and create it's socket
+        inline void createSocket();
+    };
+}
+
+#endif
---
+++ src/sockets.cpp
@@ -0,0 +1,286 @@
+/*!
+ * @file       sockets.cpp
+ * @brief      Defines everything for interfaces with OS level sockets.
+ * @author     Eddie Carle &lt;eddie@isatec.ca&gt;
+ * @date       March 4, 2016
+ * @copyright  Copyright &copy; 2016 Eddie Carle. This project is released under
+ *             the GNU General Public License Version 3.
+ *
+ * It is this file, along with sockets.hpp, that must be modified to make
+ * fastcgi++ work on any non-linux operating system.
+ */
+
+/*******************************************************************************
+* Copyright (C) 2016 Eddie Carle [eddie@isatec.ca]                             *
+*                                                                              *
+* This file is part of fastcgi++.                                              *
+*                                                                              *
+* fastcgi++ is free software: you can redistribute it and/or modify it under   *
+* the terms of the GNU Lesser General Public License as  published by the Free *
+* Software Foundation, either version 3 of the License, or (at your option) any*
+* later version.                                                               *
+*                                                                              *
+* fastcgi++ is distributed in the hope that it will be useful, but WITHOUT ANY *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR*
+* A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more    *
+* details.                                                                     *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with fastcgi++.  If not, see <http://www.gnu.org/licenses/>.           *
+*******************************************************************************/
+
+#include "fastcgi++/sockets.hpp"
+#include "fastcgi++/log.hpp"
+
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
+
+Fastcgipp::Socket::Socket(
+        const socket_t& socket,
+        Listener& listener,
+        bool valid):
+    m_data(new Data(socket, valid, listener)),
+    m_original(true)
+{
+    epoll_event event;
+
+    event.data.fd = socket;
+    event.events = EPOLLIN;
+    if(epoll_ctl(listener.m_poll, EPOLL_CTL_ADD, socket, &event) != 0)
+    {
+        WARNING_LOG("Error adding fd " << socket << " to epfd " \
+                << listener.m_poll << " using epoll_ctl(): " \
+                << std::strerror(errno))
+        close();
+    }
+}
+
+size_t Fastcgipp::Socket::read(char* buffer, size_t size)
+{
+    if(!valid())
+        return 0;
+
+    const ssize_t count = ::read(m_data->m_socket, buffer, size);
+    if(count<0)
+    {
+        WARNING_LOG("Socket read() error on fd " \
+                << m_data->m_socket << ": " << std::strerror(errno))
+        close();
+        return 0;
+    }
+    return (size_t)count;
+}
+
+size_t Fastcgipp::Socket::write(const char* buffer, size_t size)
+{
+    if(!valid())
+        return 0;
+
+    const ssize_t count = ::write(m_data->m_socket, buffer, size);
+    if(count<0)
+    {
+        WARNING_LOG("Socket write() error on fd " \
+                << m_data->m_socket << ": " << strerror(errno))
+        close();
+        return 0;
+    }
+    return (size_t)count;
+}
+
+void Fastcgipp::Socket::close()
+{
+    if(valid())
+    {
+        ::close(m_data->m_socket);
+        m_data->m_valid = false;
+        ::epoll_ctl(
+                m_data->m_listener.m_poll,
+                EPOLL_CTL_DEL,
+                m_data->m_socket,
+                nullptr);
+        m_data->m_listener.m_sockets.erase(m_data->m_socket);
+    }
+}
+
+Fastcgipp::Listener::Listener(socket_t listen):
+    m_listen(listen),
+    m_poll(epoll_create1(0)),
+    m_sleeping(false),
+    m_badSocket(-1, *this, false)
+{
+    epoll_event event;
+
+    // Add our listen socket into the epoll list
+    event.data.fd = m_listen;
+    event.events = EPOLLIN;
+    epoll_ctl(m_poll, EPOLL_CTL_ADD, m_listen, &event);
+
+    // Add our wakeup socket into the epoll list
+    socketpair(AF_UNIX, SOCK_STREAM, 0, m_wakeSockets);
+    event.data.fd = m_wakeSockets[1];
+    event.events = EPOLLIN;
+    epoll_ctl(m_poll, EPOLL_CTL_ADD, m_wakeSockets[1], &event);
+}
+
+Fastcgipp::Socket Fastcgipp::Listener::poll(bool block)
+{
+    int pollResult;
+    epoll_event event;
+
+    while(true)
+    {
+        if(block)
+        {
+            std::unique_lock<std::mutex> lock(m_sleepingMutex);
+            m_sleeping=true;
+            lock.unlock();
+            pollResult = epoll_wait(
+                    m_poll,
+                    &event,
+                    1,
+                    -1);
+            lock.lock();
+            m_sleeping=false;
+        }
+        else
+        {
+            pollResult = epoll_wait(
+                    m_poll,
+                    &event,
+                    1,
+                    0);
+        }
+
+        if(pollResult<0)
+        {
+            ERROR_LOG("Error on epoll_wait() with epfd " \
+                    << m_poll << ": " << std::strerror(errno))
+            exit(1);
+        }
+        else if(pollResult == 1)
+        {
+            if(event.data.fd == m_listen)
+            {
+                if(event.events & EPOLLIN)
+                {
+                    createSocket();
+                    continue;
+                }
+                else if(event.events & EPOLLERR)
+                {
+                    ERROR_LOG("Error in listen socket.")
+                    exit(1);
+                }
+                else if(event.events & EPOLLHUP)
+                {
+                    ERROR_LOG("Error: the listen socket hung up.")
+                    exit(1);
+                }
+            }
+            else if(event.data.fd == m_wakeSockets[1])
+            {
+                if(event.events & EPOLLIN)
+                {
+                    char x[256];
+                    read(m_wakeSockets[1], x, 256);
+                    block=false;
+                    continue;
+                }
+                else if(event.events & EPOLLHUP)
+                {
+                    ERROR_LOG("Error: the wakeup socket hung up.")
+                    exit(1);
+                }
+                else if(event.events & EPOLLERR)
+                {
+                    ERROR_LOG("Error in the wakeup socket.")
+                    exit(1);
+                }
+            }
+            else
+            {
+                if(event.events & EPOLLIN)
+                {
+                    const auto socket = m_sockets.find(event.data.fd);
+                    if(socket == m_sockets.end())
+                    {
+                        WARNING_LOG("Epoll gave fd " << event.data.fd \
+                                << " which isn't in m_sockets.")
+                        ::epoll_ctl(
+                                m_poll,
+                                EPOLL_CTL_DEL,
+                                event.data.fd,
+                                nullptr);
+                        ::close(event.data.fd);
+                        continue;
+                    }
+                    else
+                        return socket->second;
+                }
+                else if(event.events & EPOLLERR)
+                {
+                    WARNING_LOG("Error in socket" << event.data.fd << ".")
+                    m_sockets.erase(event.data.fd);
+                    continue;
+                }
+                else if(event.events & EPOLLHUP)
+                {
+                    WARNING_LOG("Socket " << event.data.fd << " hung up.")
+                    m_sockets.erase(event.data.fd);
+                    continue;
+                }
+            }
+        }
+        
+        return m_badSocket;
+    }
+}
+
+void Fastcgipp::Listener::wake()
+{
+    std::unique_lock<std::mutex> lock(m_sleepingMutex);
+    if(m_sleeping)
+    {
+        m_sleeping=false;
+        lock.unlock();
+        char x=0;
+        write(m_wakeSockets[0], &x, 1);
+    }
+}
+
+void Fastcgipp::Listener::createSocket()
+{
+    ::sockaddr_un addr;
+    ::socklen_t addrlen=sizeof(sockaddr_un);
+    const socket_t socket=::accept(
+            m_listen,
+            (::sockaddr*)&addr,
+            &addrlen);
+    if(socket<0)
+    {
+        ERROR_LOG("Error on accept() with fd " \
+                << m_listen << ": " \
+                << std::strerror(errno))
+        exit(1);
+    }
+    if(::fcntl(
+            socket,
+            F_SETFL,
+            (::fcntl(socket, F_GETFL)|O_NONBLOCK)^O_NONBLOCK)
+            < 0)
+    {
+        WARNING_LOG("Error setting NONBLOCK on fd " << socket \
+                << " with fcntl(): " << std::strerror(errno))
+        ::close(socket);
+        return;
+    }
+
+    m_sockets.erase(socket);
+    m_sockets.emplace(
+            socket,
+            std::move(Socket(socket, *this)));
+}
